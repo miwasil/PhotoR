0cde755248e041a329db27cb56d4491035996646
kasikora <karol.sikora555@gmail.com>
2023-05-06 12:02:31 +0200

    test

--- basicFuncs.py
+++ basicFuncs.py
@@ -2,7 +2,7 @@ from PIL import Image
 from customtkinter import filedialog
 
 class BasicFuncs():
-    def add_image():
+    def add_image(self):
         global file_path
         file_path = filedialog.askdirectory()
         image = Image.open(file_path)
